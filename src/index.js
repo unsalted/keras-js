27fcc2e6631052b43324679061639489c5497ec9
unsalted <nick@unsalted.nu>
2016-11-16 10:00:28 -0800

    Set keras as Class instead of Object

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import * as activations from './activations'
 import * as layers from './layers'
 import * as testUtils from './testUtils'
 
-export {
-  Model,
-  Tensor,
-  activations,
-  layers,
-  testUtils
+export class KerasJs {
+  Model = Model
+  Tensor = Tensor
+  activations = activations
+  layers = layers
+  testUtils = testUtils
 }
